8659f172378d8f625799271f43b85ac1d9c6811d
SJSL01 <simran08lall@gmail.com>
2023-02-22 14:19:54 +0530

    fixes

--- src/API/getWeatherInfo.js
+++ src/API/getWeatherInfo.js
@@ -1,6 +1,5 @@
 import axios from "axios"
 
-console.log(import.meta.env);
 export const getWeather = async (coords) => {
 
     try {
@@ -15,11 +14,7 @@ export const getWeather = async (coords) => {
 export const PlaceList = async (query) => {
 
     try {
-        const res = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5&appid=${import.meta.env.VITE_API_KEY}`)
-        console.log(res.data)
-        // const coords = [res.data[0].lat, res.data[0].lon]
-        // const response = await getWeather(coords)
-        // console.log(response.data)
+        const res = await axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5&appid=${import.meta.env.VITE_API_KEY}`)
         return res.data
     } catch (error) {
         console.log(error.message)
